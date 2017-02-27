7d41d34e14a9d63a7bc2357b6d2124fd0fe1ce6b
Vitor Capretz <vitor@idwall.co>
2017-02-27 19:58:04 -0300

    adds month flag

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const meow = require('meow')
 const updateNotifier = require('update-notifier')
 const holiday = require('holiday')
+const chalk = require('chalk')
 
 const cli = meow(`
   Usage:
@@ -15,11 +16,13 @@ const cli = meow(`
 
   Options:
     --br, --brazil       Check if today is a holiday in Brazil
+    -m, --month          Returns every holiday in current month
     -h, --help           Show help options
     -v, --version        Show version
 `, {
   alias: {
     br: 'brazil',
+    m: 'month',
     h: 'help',
     v: 'version'
   }
@@ -29,36 +32,36 @@ updateNotifier({pkg: cli.pkg}).notify()
 
 const run = () => {
   const today = new Date()
-  const day = today.getUTCDate()
+  const day = !cli.flags.m ? today.getUTCDate() : undefined
   const month = today.getUTCMonth() + 1
+  const country = cli.flags.br ? 'br' : 'us'
+  const country_emoji = cli.flags.br ? '🇧🇷' : '🇺🇸'
 
   if (cli.flags.help) {
     cli.showHelp()
-  } else if (cli.flags.br) {
-    holiday(month, day, 'br')
-      .then(res => {
-        if (res) {
-          console.log(`Today is ${res.title}! 🎊 🇧🇷`)
-
-          return false
-        }
-
-        console.log('Today isn\'t a Holiday 😔 🇧🇷')
-      })
-      .catch(err => console.log(`Ops, something went wrong... ${err}`))
-  } else {
-    holiday(month, day)
-      .then(res => {
-        if (res) {
-          console.log(`Today is ${res.title}! 🎊 🇺🇸`)
-
-          return false
-        }
-
-        console.log('Today isn\'t a Holiday 😔 🇺🇸')
-      })
-      .catch(err => console.log(`Ops, something went wrong... ${err}`))
+    return
   }
+
+  holiday(month, day, country)
+    .then(res => {
+      if (!res && cli.flags.m) {
+        console.log(`There are no Holidays this month 😔 ${country_emoji}`)
+      }
+
+      if (!res) {
+        console.log(`Today isn\'t a Holiday 😔 ${country_emoji}`)
+      }
+
+      if (res && cli.flags.m) {
+        console.log(`${chalk.bold('Holidays in January')}:\n----`)
+        Object.keys(res).map(day => console.log(`⇢ ${chalk.bold(day)}: ${res[day].title}`))
+        return
+      }
+
+      console.log(`Today is ${res.title}! 🎊 ${country_emoji}`)
+
+    })
+    .catch(err => console.log(`Ops, something went wrong... ${err}`))
 }
 
 run(cli)
