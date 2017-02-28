88f2e9c3c8a5ad18ba5d88cd4725ea9e3da640fd
Vitor Capretz <vitor@idwall.co>
2017-02-28 09:25:51 -0300

    adds month flag to show holidays in current month

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const meow = require('meow')
 const updateNotifier = require('update-notifier')
 const holiday = require('holiday')
+const chalk = require('chalk')
 
 const cli = meow(`
   Usage:
@@ -15,47 +16,69 @@ const cli = meow(`
 
   Options:
     --br, --brazil       Check if today is a holiday in Brazil
+    -m, --month          Check if today is a holiday in Brazil
     -h, --help           Show help options
     -v, --version        Show version
 `, {
-  alias: {
-    br: 'brazil',
-    h: 'help',
-    v: 'version'
-  }
-})
+    alias: {
+      br: 'brazil',
+      m: 'month',
+      h: 'help',
+      v: 'version'
+    }
+  })
 
-updateNotifier({pkg: cli.pkg}).notify()
+updateNotifier({ pkg: cli.pkg }).notify()
 
 const run = () => {
   const today = new Date()
-  const day = today.getUTCDate()
+  const day = !cli.flags.m ? today.getUTCDate() : undefined
   const month = today.getUTCMonth() + 1
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"]
 
   if (cli.flags.help) {
     cli.showHelp()
   } else if (cli.flags.br) {
     holiday(month, day, 'br')
       .then(res => {
-        if (res) {
-          console.log(`Today is ${res.title}! 🎊 🇧🇷`)
+        if (!res && cli.flags.m) {
+          console.log(`There are no Holidays this month 😔 🇧🇷`)
+        }
 
+        if (res && cli.flags.m) {
+          console.log(`${chalk.bold('Holidays in ' + monthNames[month - 1])}:\n----`)
+          Object.keys(res).map(day => console.log(`⇢ ${chalk.bold(day)}: ${res[day].title}`))
+          return
+        }
+
+        if (!res) {
+          console.log('Today isn\'t a Holiday 😔 🇧🇷')
           return false
         }
 
-        console.log('Today isn\'t a Holiday 😔 🇧🇷')
+        console.log(`Today is ${res.title}! 🎊 🇧🇷`)
       })
       .catch(err => console.log(`Ops, something went wrong... ${err}`))
   } else {
     holiday(month, day)
       .then(res => {
-        if (res) {
-          console.log(`Today is ${res.title}! 🎊 🇺🇸`)
+        if (!res && cli.flags.m) {
+          console.log(`There are no Holidays this month 😔 🇺🇸`)
+        }
 
+        if (res && cli.flags.m) {
+          console.log(`${chalk.bold('Holidays in ' + monthNames[month - 1])}:\n----`)
+          Object.keys(res).map(day => console.log(`⇢ ${chalk.bold(day)}: ${res[day].title}`))
+          return
+        }
+
+        if (res) {
+          console.log('Today isn\'t a Holiday 😔 🇺🇸')
           return false
         }
 
-        console.log('Today isn\'t a Holiday 😔 🇺🇸')
+        console.log(`Today is ${res.title}! 🎊 🇺🇸`)
       })
       .catch(err => console.log(`Ops, something went wrong... ${err}`))
   }
